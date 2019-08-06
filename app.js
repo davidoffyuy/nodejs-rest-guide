247569237d89c6ef39b1552db2cf53e3440e6bb9
David Yu <davidpyu@gmail.com>
2019-08-05 22:49:13 -0500

    Support uploading images

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const mongoose = require('mongoose');
 const MONGODB_URI = 'mongodb+srv://nodejsguide:nodejsguide@cluster0-xc044.mongodb.net/shop';
 
 // Routes
 const feedRoutes = require('./routes/feed');
 
+// Setup Multer configuration
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
 // Parser for POST text data
 app.use(bodyParser.json());
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -2,26 +2,36 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  console.log("getposts");
-  res.status(200).json({posts: [
-    {
-      _id: "testId",
-      title: "Test Title", 
-      content: "Test Content",
-      imageUrl: "images/book1.jpg",
-      creator: {
-        name: "David"
-      },
-      createdAt: new Date()
-    }
-  ]});
+  Post.find()
+  .then(posts => {
+    res.status(200).json({message: 'Posts successfully found', posts: posts});
+  })
+  .catch(error => {
+    next(error);
+  })
 }
 
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+  .then( post => {
+    if (!post) {
+      const error = new Error('Could not find post');
+      error.statusCode = 404;
+      throw error;
+    }
+    else {
+      res.status(200).json({message: 'Post fetch successful', post: post});
+    }
+  })
+  .catch(err => {
+    next(err);
+  })
+}
+
+
 exports.createPost = (req, res, next) => {
-  console.log(req.body);
-  const title = req.body.title;
-  const content = req.body.content
-  
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Invalid Input Values!');
@@ -29,10 +39,20 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  const title = req.body.title;
+  const content = req.body.content;
+  const imageUrl = req.file.path;
+
+  if (!req.file) {
+    const error = Error('Invalid Image File');
+    error.statusCode = 422;
+    throw error;
+  }
+
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/book1.jpg',
+    imageUrl: imageUrl,
     creator: {name: "David"},
   });
   post.save()
